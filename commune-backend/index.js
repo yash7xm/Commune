3a05519b471535169dfcd9711ed447024bf3f4d6
yash7xm <yash.7xm@gmail.com>
2024-03-02 16:35:29 +0530

    api initialized

--- commune-backend/index.js
+++ commune-backend/index.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 const PORT = 8080;
 
-app.get('/dog', (req, res) => {
-    res.send("wuf wuf !!")
-})
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/dog", (req, res) => {
+  res.send("wuf wuf !!");
+});
+
+app.post("/sendMessage", (req, res) => {
+  console.log(req.body.message);
+  res.sendStatus(200);
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
--- commune-frontend/src/pages/message.tsx
+++ commune-frontend/src/pages/message.tsx
@@ -1,7 +1,39 @@
+import React from "react";
+
 const Message = () => {
+  function handleMessageSent(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const message = formData.get("message");
+    console.log(message);
+
+    if (message) {
+      fetch("http://localhost:8080/sendMessage", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: message }),
+      }).then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        console.log("Message sent successfully");
+      });
+    }
+  }
+
   return (
     <div>
-      <input className="border text-lg p-3 rounded-md" type="text" />
+      <form onSubmit={handleMessageSent}>
+        <input
+          className="border text-lg p-3 rounded-md"
+          type="text"
+          name="message"
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 };
