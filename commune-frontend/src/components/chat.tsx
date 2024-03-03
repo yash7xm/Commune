259d120b059497ebe40c451a9699712f38b4dde8
yash7xm <yash.7xm@gmail.com>
2024-03-03 18:16:48 +0530

    messages added

--- commune-frontend/src/components/chat.tsx
+++ commune-frontend/src/components/chat.tsx
@@ -1,8 +1,15 @@
+import { ChevronDown } from "lucide-react";
+
 const Chat = () => {
   return (
     <div className="h-[95%] w-[25%] rounded-s-md bg-[#3F0E40] p-3">
       <div className="text-white text-start text-lg mb-8 px-2">Commune</div>
-      <div className="px-6 text-white mb-4 text-sm">Direct messages</div>
+      <div className="px-4 text-white mb-4 text-sm flex gap-1 items-center">
+        <span>
+          <ChevronDown />
+        </span>{" "}
+        <span>Direct messages</span>
+      </div>
       <div className="h-[40%] flex flex-col gap-2">
         <div className="user flex h-[15%] gap-2 items-center py-1 px-2 hover:bg-[#7A3274] rounded-sm cursor-pointer">
           <div className="user-img h-full">
--- commune-frontend/src/components/message.tsx
+++ commune-frontend/src/components/message.tsx
@@ -1,7 +1,34 @@
-const Message = () => {
-    return <div className="h-[95%] w-[70%] bg-white rounded-r-md">
+import { SendHorizontal } from "lucide-react";
 
+const Message = () => {
+  return (
+    <div className="h-[95%] w-[70%] bg-white rounded-r-md flex flex-col justify-between">
+      {/* person you are talking to or the channel name */}
+      <div className="h-[10%] border-b">
+        <div className="user flex h-full gap-2 items-center py-1 px-2">
+          <div className="user-img h-[60%]">
+            <img
+              className="h-full rounded-sm"
+              src="https://github.com/shadcn.png"
+              alt=""
+            />
+          </div>
+          <div className="font-bold text-lg">Yash Poonia</div>
+        </div>
+      </div>
+
+        <div className="flex-1">
+            <div>Messages will come here</div>
+        </div>
+
+      <div className="h-[10%] mx-4 my-2 rounded-md border">
+        <form className="h-full px-3 flex items-center">
+            <input type="text" placeholder="Message Yash Poonia" className="h-full w-[95%] outline-none flex-wrap"/>
+            <button type="submit"><SendHorizontal /></button>
+        </form>
+      </div>
     </div>
-}
+  );
+};
 
 export default Message;
