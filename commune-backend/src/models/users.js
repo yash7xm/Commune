e6cfa37450495e42579953b4d15b1f985490fa19
yash7xm <yash.7xm@gmail.com>
2024-03-04 00:45:53 +0530

    user model added

---
+++ commune-backend/src/models/users.js
@@ -0,0 +1,35 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  users.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 50],
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "users",
+    }
+  );
+  return users;
+};
