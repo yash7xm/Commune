bbf70e20b1b15d75e498c3b119b6efb6e3c36bb1
yash7xm <yash.7xm@gmail.com>
2024-03-04 00:01:06 +0530

    auth ui complete

--- commune-frontend/src/components/auth.tsx
+++ commune-frontend/src/components/auth.tsx
@@ -15,35 +15,85 @@ import {
 
 const Auth = () => {
   return (
-    <Tabs defaultValue="account" className="w-[350px]">
+    <Tabs defaultValue="register" className="w-[350px]">
       <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="account">Account</TabsTrigger>
-        <TabsTrigger value="password">Password</TabsTrigger>
+        <TabsTrigger value="register">Register</TabsTrigger>
+        <TabsTrigger value="login">Login</TabsTrigger>
       </TabsList>
-      <TabsContent value="account">
+      <TabsContent value="register">
         <Card className="w-[350px]">
           <CardHeader>
             <CardTitle>Register</CardTitle>
             <CardDescription>Create Account</CardDescription>
           </CardHeader>
           <CardContent>
-           <form className="flex flex-col gap-4">
-             <div>
-                <label htmlFor="" className="text-sm">Enter username</label>
-                <input type="text" placeholder="Username" className="border outline-none p-1 rounded-sm"/>
-             </div>
-             <div>
-                <label htmlFor="" className="text-sm">Enter Password</label>
-                <input type="text" placeholder="Password" className="border outline-none p-1 rounded-sm"/>
-             </div>
-           </form>
+            <form className="flex flex-col gap-4">
+              <div>
+                <label htmlFor="" className="text-sm">
+                  Enter username
+                </label>
+                <input
+                  type="text"
+                  placeholder="Username"
+                  className="border outline-none p-1 rounded-sm"
+                />
+              </div>
+              <div>
+                <label htmlFor="" className="text-sm">
+                  Enter Password
+                </label>
+                <input
+                  type="text"
+                  placeholder="Password"
+                  className="border outline-none p-1 rounded-sm"
+                />
+              </div>
+            </form>
           </CardContent>
           <CardFooter>
-            <button className="bg-slate-200 p-2 text-sm rounded-sm">Register</button>
+            <button className="bg-[#7A3274] text-white p-2 text-sm rounded-sm">
+              Register
+            </button>
           </CardFooter>
         </Card>
       </TabsContent>
-      <TabsContent value="password">Change your password here.</TabsContent>
+      <TabsContent value="login">
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>LogIn</CardTitle>
+            <CardDescription>Welcome Back</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form className="flex flex-col gap-4">
+              <div>
+                <label htmlFor="" className="text-sm">
+                  Enter username
+                </label>
+                <input
+                  type="text"
+                  placeholder="Username"
+                  className="border outline-none p-1 rounded-sm"
+                />
+              </div>
+              <div>
+                <label htmlFor="" className="text-sm">
+                  Enter Password
+                </label>
+                <input
+                  type="text"
+                  placeholder="Password"
+                  className="border outline-none p-1 rounded-sm"
+                />
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter>
+            <button className="bg-[#7A3274] text-white  p-2 text-sm rounded-sm">
+              Login
+            </button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
     </Tabs>
   );
 };
