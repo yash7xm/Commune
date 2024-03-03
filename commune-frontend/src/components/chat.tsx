ad3b7b5492052510451dd522a751e383aa0f0bca
yash7xm <yash.7xm@gmail.com>
2024-03-03 17:25:58 +0530

    chats ui added

--- commune-frontend/src/components/chat.tsx
+++ commune-frontend/src/components/chat.tsx
@@ -1,12 +1,29 @@
 const Chat = () => {
   return (
     <div className="h-[95%] w-[25%] rounded-s-md bg-[#3F0E40] p-3">
-      <div className="text-white text-start text-lg mb-8">Commune</div>
-      <div className="user flex h-[5%] gap-2 items-center">
-        <div className="user-img h-full rounded-lg">
-          <img className="h-full rounded-lg" src="https://github.com/shadcn.png" alt="" />
+      <div className="text-white text-start text-lg mb-8 px-2">Commune</div>
+      <div className="px-6 text-white mb-4 text-sm">Direct messages</div>
+      <div className="h-[40%] flex flex-col gap-2">
+        <div className="user flex h-[15%] gap-2 items-center py-1 px-2 hover:bg-[#7A3274] rounded-sm cursor-pointer">
+          <div className="user-img h-full">
+            <img
+              className="h-full rounded-sm"
+              src="https://github.com/shadcn.png"
+              alt=""
+            />
+          </div>
+          <div className="user-name text-white text-sm">Yash Poonia</div>
         </div>
-        <div className="user-name text-white text-sm">Yash Poonia</div>
+        <div className="user flex h-[15%] gap-2 items-center py-1 px-2 hover:bg-[#7A3274] rounded-sm cursor-pointer">
+          <div className="user-img h-full">
+            <img
+              className="h-full rounded-sm"
+              src="https://github.com/shadcn.png"
+              alt=""
+            />
+          </div>
+          <div className="user-name text-white text-sm">Yash Poonia</div>
+        </div>
       </div>
     </div>
   );
