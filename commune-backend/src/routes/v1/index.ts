49edd114242b5a3577a9edc2c85afb5bb097d2ee
yash7xm <yash.7xm@gmail.com>
2024-03-04 12:50:44 +0530

    user routes added

--- commune-backend/src/routes/v1/index.ts
+++ commune-backend/src/routes/v1/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import messageRoutes from "./message-routes";
 
+const { userRoutes } = require("./user-routes");
+
 const router = express.Router();
 
 router.use("/msg", messageRoutes);
+
+router.use("/user", userRoutes);
 
 export default router;
