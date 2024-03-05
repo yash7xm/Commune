582a1ae606ce44a75031d6c7055a21cb5c65e1ea
yash7xm <yash.7xm@gmail.com>
2024-03-05 15:33:59 +0530

    add user dialog fix

--- commune-frontend/src/components/addUserDialog.tsx
+++ commune-frontend/src/components/addUserDialog.tsx
@@ -12,12 +12,21 @@ import {
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
+import { useState } from "react";
 
 const AddUserDialog = () => {
+  const [name, setName] = useState("Pedro Duarte");
+  const [username, setUsername] = useState("@peduarte");
+
+  const handleAddUserFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("form submitted with name:", name, "and username:", username);
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild className="cursor-pointer">
-          <UserRoundPlus size={18} />
+        <UserRoundPlus size={18} />
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
@@ -26,23 +35,35 @@ const AddUserDialog = () => {
             Add your friend to have a chat. Click add when you're done.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+        <form onSubmit={handleAddUserFormSubmit}>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Username
+              </Label>
+              <Input
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="col-span-3"
+              />
+            </div>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Add</Button>
-        </DialogFooter>
+          <DialogFooter>
+            <Button type="submit">Add</Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
