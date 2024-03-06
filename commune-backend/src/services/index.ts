511ba79e95821d635f51d6b13eb1713ecb0fc62a
yash7xm <yash.7xm@gmail.com>
2024-03-06 12:28:06 +0530

    send message services added

--- commune-backend/src/services/index.ts
+++ commune-backend/src/services/index.ts
@@ -1,4 +1,5 @@
 module.exports = {
   UserService: require("./user-service"),
-  ChannelService: require("./channel-services")
+  ChannelService: require("./channel-services"),
+  MessageService: require("./message-services"),
 };
---
+++ commune-backend/src/services/message-services.ts
@@ -0,0 +1,31 @@
+const { MessageRespository } = require("../repositories");
+import { StatusCodes } from "http-status-codes";
+const { AppError } = require("../utils");
+
+const messageRepo = new MessageRespository();
+
+async function sendMessage(data: any) {
+  try {
+    const message = await messageRepo.create(data);
+    return message;
+  } catch (error: any) {
+    if (
+      error.name == "SequelizeValidationError" ||
+      error.name == "SequelizeUniqueConstraintError"
+    ) {
+      let explanation: any = [];
+      error.errors.forEach((err: any) => {
+        explanation.push(err.message);
+      });
+      throw new AppError(explanation, StatusCodes.BAD_REQUEST);
+    }
+    throw new AppError(
+      "Cannot send a new message",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+module.exports = {
+  sendMessage,
+};
